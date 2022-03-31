36a287af81e58e422273c98cb2a7bac81ccf7ff2
Shawn Allen <shawnbot@users.noreply.github.com>
2022-03-31 16:29:57 -0700

    chore: add more useful output to mock app

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,18 @@ import express, { Application, Request, Response } from 'express'
 
 const { PORT = 80 } = process.env
 const app: Application = express()
-  .get('/', (req: Request, res: Response) => {
-    res.send('Hello from Express!')
+  .use((req: Request, res: Response) => {
+    res.send(`
+      <h1>Hello from Express!</h1>
+      <dl>
+        <dt>Host</dt>
+        <dd><code>${req.hostname}</code></dd>
+        <dt>url</dt>
+        <dd><code>${req.url}</code></dd>
+        <dt>headers</dt>
+        <dd><code><pre>${JSON.stringify(req.headers, null, 2)}</pre></code></dd>
+      </dl>
+    `)
   })
 
 const server = app.listen(PORT, () => {
