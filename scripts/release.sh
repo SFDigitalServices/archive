6b0e64c1b17c847dfe71d4b777877aa990c82793
Shawn Allen <shawnbot@users.noreply.github.com>
2022-04-14 12:55:09 -0700

    fix(heroku): tweak more release script bits

--- scripts/release.sh
+++ scripts/release.sh
@@ -10,11 +10,13 @@ if [ "$HEROKU_APP_NAME" = "sfgov-archive" ]; then
   exit
 fi
 
+echo "Checking Heroku auth..."
+npx heroku auth:whoami
 echo "Setting up auto-domains for $HEROKU_APP_NAME..."
-for host in `perl -ne 'print "$1\n" if /ServerAlias .*HEROKU_APP_NAME}?\.(.+)"?$/;' httpd/conf/**/*.conf`; do
+for host in `perl -ne 'print "$1\n" if /ServerAlias .*HEROKU_APP_NAME}?\.([^"\s$]+)/;' httpd/conf/**/*.conf`; do
   domain="$HEROKU_APP_NAME.$host"
   npx heroku domains:add -a "$HEROKU_APP_NAME" "$domain" \
     || echo "Unable to add domain '$domain' to app '$HEROKU_APP_NAME'"
 done
 echo "Waiting for auto-domains..."
-npx heroku domains:wait
+npx heroku domains:wait -a "$HEROKU_APP_NAME"
