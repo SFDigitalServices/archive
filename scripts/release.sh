04034d29f82cacfe03f11f7274e5aeb42edfc9a5
Shawn Allen <shawnbot@users.noreply.github.com>
2022-04-14 12:46:50 -0700

    fix(heroku): wait for domains

--- scripts/release.sh
+++ scripts/release.sh
@@ -16,3 +16,5 @@ for host in `perl -ne 'print "$1\n" if /ServerAlias .*HEROKU_APP_NAME}?\.(.+)"?$
   npx heroku domains:add -a "$HEROKU_APP_NAME" "$domain" \
     || echo "Unable to add domain '$domain' to app '$HEROKU_APP_NAME'"
 done
+echo "Waiting for auto-domains..."
+npx heroku domains:wait
