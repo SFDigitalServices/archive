b48fabfa044b97fcda1cd3dc580d9f587d83510b
Shawn Allen <shawnbot@users.noreply.github.com>
2022-04-14 11:38:04 -0700

    chore(heroku): install heroko cli from npm

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,8 +5,9 @@ if [ -z "$HEROKU_APP_NAME" ]; then
   exit
 fi
 
+npm i -g heroku
 for host in `perl -ne 'print "$1\n" if /ServerAlias "?\*\.(.+)"?$/;' httpd/conf/**/*.conf`; do
   domain="$HEROKU_APP_NAME.$host"
-  heroku domains:add -a "$HEROKU_APP_NAME" "$domain" \
+  npx heroku domains:add -a "$HEROKU_APP_NAME" "$domain" \
     || echo "Unable to add domain '$domain' to app '$HEROKU_APP_NAME'"
 done
