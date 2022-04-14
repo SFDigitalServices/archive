05e431215553c59021d379f938612590dd1e45d0
Shawn Allen <shawnbot@users.noreply.github.com>
2022-04-14 12:07:00 -0700

    fix(heroku): look for $HEROKU_APP_NAME.domain, not *.domain

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,7 @@ if [ -z "$HEROKU_APP_NAME" ]; then
   exit
 fi
 
-for host in `perl -ne 'print "$1\n" if /ServerAlias "?\*\.(.+)"?$/;' httpd/conf/**/*.conf`; do
+for host in `perl -ne 'print "$1\n" if /ServerAlias .*HEROKU_APP_NAME}?\.(.+)"?$/;' httpd/conf/**/*.conf`; do
   domain="$HEROKU_APP_NAME.$host"
   npx heroku domains:add -a "$HEROKU_APP_NAME" "$domain" \
     || echo "Unable to add domain '$domain' to app '$HEROKU_APP_NAME'"
