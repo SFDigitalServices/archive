7faef8777b68fbfe10f32de81435e45ed0438abc
Shawn Allen <shawnbot@users.noreply.github.com>
2022-04-14 12:34:48 -0700

    chore(heroku): output some useful info in release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,8 +5,14 @@ if [ -z "$HEROKU_APP_NAME" ]; then
   exit
 fi
 
+if [ "$HEROKU_APP_NAME" -eq "sfgov-archive" ]; then
+  echo "No auto-domains to set in production"
+  exit
+fi
+
+echo "Setting up auto-domains for $HEROKU_APP_NAME..."
 for host in `perl -ne 'print "$1\n" if /ServerAlias .*HEROKU_APP_NAME}?\.(.+)"?$/;' httpd/conf/**/*.conf`; do
   domain="$HEROKU_APP_NAME.$host"
-  npx heroku domains:add -a "$HEROKU_APP_NAME" "$domain" \
+  echo npx heroku domains:add -a "$HEROKU_APP_NAME" "$domain" \
     || echo "Unable to add domain '$domain' to app '$HEROKU_APP_NAME'"
 done
