5fd79906fc2ec721ab60ca1d0427f008d0836c1b
Shawn Allen <shawnbot@users.noreply.github.com>
2022-07-01 15:53:44 -0700

    feat(develop): add types.ts

---
+++ types.ts
@@ -0,0 +1,22 @@
+export type ArchiveMetadata = {
+  collection_id: number | string
+  base_url: string
+}
+
+export type RedirectMapEntry = {
+  map: Record<string, string>
+}
+
+export type RedirectFileEntry = {
+  file: string
+  type?: string
+}
+
+export type RedirectEntry = RedirectMapEntry | RedirectFileEntry
+
+export type SiteConfigData = {
+  path: string
+  archive: ArchiveMetadata
+  hostnames?: string[]
+  redirects?: RedirectEntry[]
+}
