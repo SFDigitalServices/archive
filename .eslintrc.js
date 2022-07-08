c4481bdd06c1d9bbd4d54965a478e0cd542755be
Shawn Allen <shawnbot@users.noreply.github.com>
2022-07-08 12:41:01 -0700

    chore(eslint): add some unicorn linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,17 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  plugins: ['sfgov'],
+  plugins: [
+    'sfgov',
+    'unicorn'
+  ],
   extends: [
     'plugin:sfgov/recommended',
     'plugin:sfgov/node'
   ],
   rules: {
+    'unicorn/prefer-at': 1,
+    'unicorn/better-regex': 1,
+    'unicorn/no-unsafe-regex': 1
   },
   reportUnusedDisableDirectives: true,
   overrides: [
