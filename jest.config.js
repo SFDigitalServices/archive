9b796278f7aff9acbbb2e5b29c12814f44397b49
Shawn Allen <shawnbot@users.noreply.github.com>
2022-08-11 12:05:09 -0700

    chore(tests): set coverage threshold to 100% of lines

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 /** @type {import('jest').Config} */
 module.exports = {
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      lines: 100
+    }
+  },
   setupFiles: [
     './lib/test-setup.js'
   ]
